7b128657fe449b43a0dd89d594655aa058ef6845
Серега ReDetection <ReDetection@211.ru>
2010-05-26 15:01:51 +0700

    created waveeffect

---
+++ WaveEffect.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   WaveEffect.cpp
+ * Author: rd
+ *
+ * Класс, от которого будут наследоваться звуковые эффекты.
+ *
+ * Created on 26 Май 2010 г., 14:36
+ */
+
+#include "WaveEffect.h"
+
+WaveEffect::WaveEffect() {
+    i=0;
+}
+
+WaveEffect::WaveEffect(const WaveEffect& orig) {
+}
+
+WaveEffect::~WaveEffect() {
+}
+
---
+++ WaveEffect.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   WaveEffect.h
+ * Author: rd
+ *
+ * Created on 26 Май 2010 г., 14:36
+ */
+
+#ifndef _WAVEEFFECT_H
+#define	_WAVEEFFECT_H
+
+class WaveEffect {
+public:
+    WaveEffect();
+    WaveEffect(const WaveEffect& orig);
+    virtual ~WaveEffect();
+private:
+
+    int i;
+
+};
+
+#endif	/* _WAVEEFFECT_H */
+
