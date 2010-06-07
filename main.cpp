77c0babb8db52d32cf5452d256c9f9cd35254ed4
Серега ReDetection <ReDetection@211.ru>
2010-06-07 20:33:33 +0700

    убрал мусор из Мейна

--- main.cpp
+++ main.cpp
@@ -13,53 +13,7 @@
 /*
  * 
  */
-int main(int argc, char** argv) {
-////    StringArray array;
-////    array.addConst("расрас");
-////    array.addConst("двадва");
-////    array.putConst("полтора",1);
-////    array.raiseUp(2);
-////    for(int i=array.getSize()-1;i>=0;i--)
-////        printf("%s\n",array[i]);
-////    std::ofstream of;
-////    of.open("123",std::ios::binary | std::ios::out);
-////    array.store(of);
-////    of.close();
-////
-////    StringArray aa;
-////    std::ifstream in;
-////    in.open("123",std::ios::binary | std::ios::in);
-////    aa.load(in);
-////    for(int i=aa.getSize()-1;i>=0;i--)
-////        printf("%s\n",aa[i]);
-//
-////    Menu menu(&array,"Так вот:");
-////
-////    menu.chooseIndex();
-//
-//
-////    FileSelectorUI fselector;
-////    printf("\n\n%s\n",fselector.select());
-//
-//    try{
-//        WaveFile wave("/home/rd/Desktop/свалка/audiodump.wav");
-////        WaveFile wave("/tmp/2.wav");
-////        WaveFile wave("/tmp/lin1.wav");
-////        wave.applyEffect(new Compressor(0.5,0.75));
-////        wave.applyEffect(new Overdrive());
-////        wave.applyEffect(new Distortion());
-////        wave.applyEffect(new Amplifier(0.5));
-////        wave.applyEffect(new Echo(0.025,0.8));
-//        wave.applyEffect(new Distortion());
-////        wave.applyEffect(new Overdrive());
-////        wave.applyEffect(new Echo(0.07,0.5));
-////        wave.applyEffect(new Echo(0.12,0.3));
-//        wave.store("/tmp/3.wav");
-//    }catch(const char* msg){
-//        printf(msg);
-////        throw msg;
-//    }
-
+int main(int argc, char** argv){
 
     FileSelectorUI selector;
 
